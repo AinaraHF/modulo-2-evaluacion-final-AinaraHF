187c37022c7e22b2f938e0252cebe80d5d95e8fa
AinaraHF <ainarahf@gmail.com>
2025-01-15 13:10:04 +0100

    Second part done and halfway through third one, favorites

--- src/js/main.js
+++ src/js/main.js
@@ -3,28 +3,15 @@
 const searchInput = document.querySelector('.js-search_input');
 const searchBtn = document.querySelector('.js-search_btn');
 const searchList = document.querySelector('.js-search_list');
-
-/*<li>
-        <img src="" alt=""/>
-        <p></p>
-  </li>
-const li = document.createElement('li');
-li.append(image,h3);
-searchList.appendChild(li);
-const image = document.createElement('img');
-image.setAttribute("src", item.img); //cambiar item.img por el info.data.img
-const h3 = document.createElement('h3');
-const textH3 = document.createTextNode(info.data.title); //cambiar item.img por el info.data.img
-h3.appendChild(textH3);
-
-  */
+const favList = document.querySelector('.js-fav_list');
 
 let searchSeries = [];
+let favSeries = [];
 
-function handleSearch(ev){
-    ev.preventDefault();
-    const inputValue = searchInput.value;
-    fetch(`https://api.jikan.moe/v4/anime? q=${inputValue}`)
+function handleSearch(ev) {
+  ev.preventDefault();
+  const inputValue = searchInput.value;
+  fetch(`https://api.jikan.moe/v4/anime?q=${inputValue}`)
     .then((response) => response.json())
     .then((info) => {
       searchSeries = info.data;
@@ -34,19 +21,52 @@ function handleSearch(ev){
 
 searchBtn.addEventListener('click', handleSearch);
 
-function renderSeries(series){
+function renderSeries(series) {
   for (const serie of series) {
-  const li = document.createElement('li');
-  searchList.appendChild(li);
-  const image = document.createElement('img');
-  image.setAttribute("src", serie.images.jpg.image_url); 
-  const h3 = document.createElement('h3');
-  const textH3 = document.createTextNode(serie.title); 
-  h3.appendChild(textH3);
-  li.append(image,h3);
-  }
-};
+    const li = document.createElement('li');
+    li.setAttribute('id', serie.mal_id);
+    li.setAttribute('class', 'js-li');
+    searchList.appendChild(li);
+    const image = document.createElement('img');
+    const h3 = document.createElement('h3');
+    const textH3 = document.createTextNode(serie.title);
+    h3.appendChild(textH3);
+    li.append(image, h3);
+    if (serie.images.webp.image_url === `https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png`) {
+      image.setAttribute('src', 'https://placehold.co/210x295/ffffff/666666/png');
+    } else {
+      image.setAttribute('src', serie.images.webp.image_url);
+    }
+    /*const findFav = favSeries.find((favSerie) => favSerie.id === serie.id);
 
+    let css = findFav ? 'favorite' : '';
+
+    favList.innerHTML += `
+    <li id="${serie.id}" class="js_palette ${css}">
+        <img src="${serie.serie.images.webp.image_url}" alt="">
+      <h3>${serie.title}</h3>
+    </li>`;*/
+  }
+    listenerSeries();
+  };
+
+
+  function handleClickFav(event) {
+    const liClicked = parseInt(event.currentTarget.id);
+    const serieSelected = searchSeries.find((eachSerie) => eachSerie.mal_id === liClicked);
+    const indexFavSelected = favSeries.findIndex((searchSerie) => searchSerie.id === liClicked);
+    if (indexFavSelected === -1) {
+      favSeries.push(serieSelected);};
+      console.log(favSeries); //no funciona lo de ke no se repitan...
+    renderSeries(searchSeries);
+}
+
+  const listenerSeries = () => {
+    const allSeriesLi = document.querySelectorAll('.js-li');
+    for (const li of allSeriesLi) {
+      li.addEventListener('click', handleClickFav);
+    }
+  };
 
 
 
